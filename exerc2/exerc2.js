9c798d5e549805abc4cbb1137a0133c321c2180b
Gabriel Pineiro <grpineiro@gmail.com>
2019-09-14 21:50:38 -0300

    Updated Exercise 2

--- exerc2/exerc2.js
+++ exerc2/exerc2.js
@@ -1,22 +1,22 @@
 /** A função ira retorna um valor somado com o seu anterior. */
-function Fibonacci(number) {            // Inserindo um valor
-    if (number < 1) return 0;
-    if (number <= 2) return 1;
-    
-    let fato2 = 0;
-    let fato1 = 1;
-    let fatoN = number;
+function Fibonacci() {            // Inserindo um valor
+    let fib = [0,1];
+    let i = 0;
+    let soma = 0;
 
-    console.log(0);
-    console.log(1);
-    for (let i = 2; i <= number; i++) {
-        fatoN = fato1 + fato2;
-        fato2 = fato1;
-        fato1 = fatoN;
-        console.log(fatoN)
+    while (fib[0]+fib[1] < 4000000) {
+        i = fib[0] + fib[1];
+        fib[0] = fib[1];
+        fib[1] = i;
+    
+            if(i % 2 === 0){
+            soma += i;
+        }
     }
-    return fatoN;
+
+    return soma;
 }
 
+console.log(Fibonacci())
 let tela = document.querySelector("#telaFibonacci");
-tela.innerHTML = `Os números somados é igual a ${Fibonacci(10)}`
+tela.innerHTML = `Os números somados é igual a ${Fibonacci()}`
